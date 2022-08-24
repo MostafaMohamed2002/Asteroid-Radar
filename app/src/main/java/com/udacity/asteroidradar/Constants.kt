47eef65e91cf7f642270ffb3f804d06bc623b784
MostafaMohamed2002 <Mostafa0Mohamed2002@gmail.com>
2022-08-24 11:47:50 +0200

    delete api key so user can add his own

--- app/src/main/java/com/udacity/asteroidradar/Constants.kt
+++ app/src/main/java/com/udacity/asteroidradar/Constants.kt
@@ -7,7 +7,9 @@ object Constants {
     const val API_QUERY_DATE_FORMAT = "YYYY-MM-dd"
     const val DEFAULT_END_DATE_DAYS = 7
     const val BASE_URL = "https://api.nasa.gov/"
-    const val API_KEY="N2Sp1Eoqv5aJ2Rf81mM32wvNZNRbi8CF9xk3uVUF"
+    //add your api key down below
+    const val API_KEY=""
+
 
 
     fun getCurrentDate(): String {
